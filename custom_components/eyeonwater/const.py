4a1663e6dd91d57ab38a851a3fc510095a7c1d78
Kostya Deev <kostya.deev@bluware.com>
2021-12-17 16:04:33 -0600

    new timeouts

--- custom_components/eyeonwater/const.py
+++ custom_components/eyeonwater/const.py
@@ -1,8 +1,8 @@
 """Constants for the Eye On Water integration."""
 from datetime import timedelta
 
-SCAN_INTERVAL = timedelta(minutes=1)
-DEBOUNCE_COOLDOWN = 5*60  # Seconds
+SCAN_INTERVAL = timedelta(minutes=5)
+DEBOUNCE_COOLDOWN = 15*60  # Seconds
 
 DATA_COORDINATOR = "coordinator"
 DATA_SMART_METER = "smart_meter_data"

09f10136276dae95a8153c1e24d0160626b7eade
Konstantin Deev <kn.deev@gmail.com>
2022-11-25 11:06:53 -0600

    Update config_flow.py

--- custom_components/eyeonwater/config_flow.py
+++ custom_components/eyeonwater/config_flow.py
@@ -32,10 +32,11 @@ DATA_SCHEMA = vol.Schema(
     {
         CONF_EOW_HOSTNAME: selector({"select": {"options": [CONF_EOW_HOSTNAME_COM, CONF_EOW_HOSTNAME_CA]}}),
         CONF_MEASUREMENT_SYSTEM: selector({"select": {"options": [CONF_MEASUREMENT_SYSTEM_METRIC, CONF_MEASUREMENT_SYSTEM_IMPERIAL]}}),
-        vol.Required(CONF_USERNAME, default="username"): str, 
+        vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
     }
 )
+
 
 def create_account_from_config(data: Dict[str, Any]) -> Account:
 
@@ -52,8 +53,8 @@ def create_account_from_config(data: Dict[str, Any]) -> Account:
         eow_hostname = CONF_EOW_HOSTNAME_CA
         metric_measurement_system = True
     else:
-        raise WrongDomain(f"Unsupported domain {domain}. Only 'com' and 'ca' are supported")
-
+        raise Exception()(
+            f"Unsupported domain {domain}. Only 'com' and 'ca' are supported")
 
     # Measurement system
     try:
@@ -68,11 +69,13 @@ def create_account_from_config(data: Dict[str, Any]) -> Account:
     except KeyError:
         pass
 
-    username=data[CONF_USERNAME]
-    password=data[CONF_PASSWORD]
+    username = data[CONF_USERNAME]
+    password = data[CONF_PASSWORD]
 
-    account = Account(eow_hostname=eow_hostname, username=username, password=password, metric_measurement_system=metric_measurement_system)
+    account = Account(eow_hostname=eow_hostname, username=username,
+                      password=password, metric_measurement_system=metric_measurement_system)
     return account
+
 
 async def validate_input(hass: core.HomeAssistant, data):
     """Validate the user input allows us to connect.
@@ -108,8 +111,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 info = await validate_input(self.hass, user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
-            # except WrongDomain:
-            #     errors["base"] = "wrong_domain"
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
             except Exception:  # pylint: disable=broad-except
@@ -126,9 +127,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
-
-class WrongDomain(exceptions.HomeAssistantError):
-    """Error to indicate wrong EOW domain."""
 
 
 class CannotConnect(exceptions.HomeAssistantError):

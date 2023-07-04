bb60644d83e6cee625910ef5047614b8f303b85a
Konstantin Deev <konstantinde@zillowgroup.com>
2023-07-03 23:16:31 -0500

    extend test.py

--- custom_components/eyeonwater/test.py
+++ custom_components/eyeonwater/test.py
@@ -5,13 +5,14 @@ import asyncio
 from eow import Account, Client, Meter
 
 async def main():
-    account = Account(username="_", password="_")
+    account = Account(eow_hostname="eyeonwater.com", username="your EOW login", password="your EOW password", metric_measurement_system=False)
     websession = aiohttp.ClientSession()
     client = Client(websession=websession, account=account)
 
     await client.authenticate()
         
     meters = await account.fetch_meters(client=client)
+    print(f"{len(meters)} meters found")
     for meter in meters:
         await meter.read_meter(client=client)
         print(f"meter {meter.meter_id} shows {meter.reading}")

7b1a44e28ec1ded685cd5fac405685ea53e0b3fb
Konstantin Deev <kn.deev@gmail.com>
2023-07-13 12:33:14 -0500

    support for CUBIC_FEET unit

--- custom_components/eyeonwater/eow.py
+++ custom_components/eyeonwater/eow.py
@@ -23,7 +23,7 @@ SEARCH_ENDPOINT = "/api/2/residential/new_search"
 MEASUREMENT_GALLONS = "GAL"
 MEASUREMENT_100_GALLONS = "100 GAL"
 MEASUREMENT_10_GALLONS = "10 GAL"
-MEASUREMENT_CF = "CF"
+MEASUREMENT_CF = ["CF", "CUBIC_FEET"]
 MEASUREMENT_CCF = "CCF"
 MEASUREMENT_KILOGALLONS = "KGAL"
 MEASUREMENT_CUBICMETERS = "CM"
@@ -136,7 +136,7 @@ class Meter:
                 pass
             elif read_unit.upper() == MEASUREMENT_CCF:
                 amount = amount * 748.052
-            elif read_unit.upper() == MEASUREMENT_CF:
+            elif read_unit.upper() in MEASUREMENT_CF:
                 amount = amount * 7.48052
             else:
                 raise EyeOnWaterAPIError(f"Unsupported measurement unit: {read_unit}")

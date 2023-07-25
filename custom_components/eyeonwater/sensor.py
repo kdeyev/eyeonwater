bed69535cae60b011d2dce4aed98d05a322a350e
disforw <github@benzona.net>
2023-07-25 18:42:27 -0400

    sensor.py - Remove leak detector
    Remove leak detector class to move to binary_sensor

--- custom_components/eyeonwater/sensor.py
+++ custom_components/eyeonwater/sensor.py
@@ -8,17 +8,11 @@ from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-)
-
 
 from .const import (
     DATA_COORDINATOR,
     DATA_SMART_METER,
     DOMAIN,
-    WATER_LEAK_SENSOR,
     WATER_METER,
     DEVICE_CLASS_WATER
 )
@@ -32,7 +26,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     sensors = []
     for meter in meters:
         sensors.append(EyeOnWaterSensor(meter, coordinator))
-        sensors.append(EyeOnWaterLeakSensor(meter, coordinator))
 
     async_add_entities(sensors, False)
 
@@ -83,73 +76,6 @@ class EyeOnWaterSensor(CoordinatorEntity, RestoreEntity, SensorEntity):
         self._available = self.coordinator.last_update_success
         if self._available:
             self._state = self.meter.reading
-        self.async_write_ha_state()
-
-    async def async_added_to_hass(self):
-        """Subscribe to updates."""
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self._state_update))
-
-        # If the background update finished before
-        # we added the entity, there is no need to restore
-        # state.
-        if self.coordinator.last_update_success:
-            return
-
-        if last_state := await self.async_get_last_state():
-            self._state = last_state.state
-            self._available = True
-
-
-class EyeOnWaterLeakSensor(CoordinatorEntity, RestoreEntity, BinarySensorEntity):
-    """Representation of an Eye On Water leak sensor."""
-
-    _attr_device_class = BinarySensorDeviceClass.PROBLEM
-
-    def __init__(self, meter: Meter, coordinator: DataUpdateCoordinator) -> None:
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.meter = meter
-        self._state = None
-        self._available = False
-
-    @property
-    def name(self):
-        """Device Name."""
-        return f"{WATER_LEAK_SENSOR} {self.meter.meter_uuid}"
-
-    @property
-    def unique_id(self):
-        """Device Uniqueid."""
-        return f"leak_{self.meter.meter_uuid}"
-
-    @property
-    def available(self):
-        """Return True if entity is available."""
-        return self._available
-
-    @property
-    def native_value(self):
-        """Get the latest reading."""
-        return self._state
-
-    @property
-    def is_on(self):
-        """Return the status of the sensor."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        """Return the device specific state attributes."""
-        attributes = self.meter.attributes
-        return attributes
-
-    @callback
-    def _state_update(self):
-        """Call when the coordinator has an update."""
-        self._available = self.coordinator.last_update_success
-        if self._available:
-            self._state = self.meter.has_leak
         self.async_write_ha_state()
 
     async def async_added_to_hass(self):

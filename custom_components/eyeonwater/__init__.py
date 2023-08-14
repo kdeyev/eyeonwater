27ea44392b8ea2c8f0541d11acab25d1b90d361e
disforw <github@benzona.net>
2023-08-14 00:08:09 -0400

    Update __init__.py

--- custom_components/eyeonwater/__init__.py
+++ custom_components/eyeonwater/__init__.py
@@ -66,37 +66,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-class EyeOnWaterData:
-    """Manages coordinatation of API data updates."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        entry: ConfigEntry,
-        account: Account,
-    ) -> None:
-        """Initialize the data coordintator."""
-        self._entry = entry
-        self.account = account
-        websession = aiohttp_client.async_get_clientsession(hass)
-        self.client = Client(websession, account)
-        self.meters: list = []
-
-    async def setup(self):
-        """Fetch all of the user's meters."""
-        self.meters = await self.account.fetch_meters(self.client)
-        _LOGGER.debug("Discovered %s meter(s)", len(self.meters))
-
-    async def read_meters(self):
-        """Read each meter."""
-        for meter in self.meters:
-            try:
-                await meter.read_meter(self.client)
-            except (EyeOnWaterAPIError, EyeOnWaterAuthError) as error:
-                raise UpdateFailed(error) from error
-        return self.meters
-
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)

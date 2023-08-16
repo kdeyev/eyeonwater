e97672bac47da6f6a5e3a0d8ead73159ccf8ac47
Konstantin Deev <konstantinde@zillowgroup.com>
2023-08-16 09:21:37 -0500

    optimize import

--- custom_components/eyeonwater/coordinator.py
+++ custom_components/eyeonwater/coordinator.py
@@ -1,6 +1,7 @@
 """EyeOnWater coordinator."""
 import logging
 import datetime
+from typing import List
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -16,6 +17,7 @@ from .config_flow import create_account_from_config
 from .eow import (
     Account,
     Client,
+    Meter,
     EyeOnWaterAPIError,
     EyeOnWaterAuthError,
     EyeOnWaterResponseIsEmpty,
@@ -57,46 +59,14 @@ class EyeOnWaterData:
         return self.meters
 
     async def import_historical_data(self, days_to_load: int = 2):
-        """Import data for today and past N days."""
+        """Import historical data for today and past N days."""
 
-        today = datetime.datetime.now().replace(
-            hour=0, minute=0, second=0, microsecond=0
-        )
+        for meter in self.meters:
+            statistics = await self.get_historical_data(meter, days_to_load)
 
-        date_list = [today - datetime.timedelta(days=x) for x in range(0, days_to_load)]
-        for date in date_list:
-            for meter in self.meters:
+            if statistics:
                 name = f"{WATER_METER_NAME} {meter.meter_uuid}"
                 statistic_id = name = f"sensor.water_meter_{meter.meter_uuid}"
-
-                # TODO: it was tested with "gal" only
-                units = meter.native_unit_of_measurement.upper()
-
-                _LOGGER.info(
-                    f"adding historical statistics for {name} on {date} with units {units}"
-                )
-
-                try:
-                    data = await meter.get_historical_data(
-                        date=date, units=units, client=self.client
-                    )
-                except EyeOnWaterResponseIsEmpty:
-                    # Suppress this exception. It's valid situation when data was not reported by EOW for the requested day
-                    continue
-                except (EyeOnWaterAPIError, EyeOnWaterAuthError) as error:
-                    raise UpdateFailed(error) from error
-
-                statistics = []
-                for row in data:
-                    _LOGGER.debug(row)
-                    statistics.append(
-                        StatisticData(
-                            start=row["start"],
-                            sum=row["sum"],
-                            min=row["sum"],
-                            max=row["sum"],
-                        )
-                    )
 
                 metadata = StatisticMetaData(
                     has_mean=False,
@@ -104,6 +74,53 @@ class EyeOnWaterData:
                     name=name,
                     source="recorder",
                     statistic_id=statistic_id,
-                    unit_of_measurement=meter.native_unit_of_measurement.upper(),
+                    unit_of_measurement=meter.native_unit_of_measurement,
                 )
                 async_import_statistics(self.hass, metadata, statistics)
+
+    async def get_historical_data(
+        self, meter: Meter, days_to_load: int = 2
+    ) -> List[StatisticData]:
+        """Retrieve historical data for today and past N days."""
+
+        today = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+
+        date_list = [today - datetime.timedelta(days=x) for x in range(0, days_to_load)]
+
+        # TODO: it was tested with "GAL" only
+        units = meter.native_unit_of_measurement.upper()
+
+        _LOGGER.info(
+            f"adding historical statistics for {meter.meter_uuid} on {date_list} with units {units}"
+        )
+
+        statistics = []
+
+        for date in date_list:
+            _LOGGER.debug(
+                f"requesting historical statistics for {meter.meter_uuid} on {date} with units {units}"
+            )
+            try:
+                data = await meter.get_historical_data(
+                    date=date, units=units, client=self.client
+                )
+            except EyeOnWaterResponseIsEmpty:
+                # Suppress this exception. It's valid situation when data was not reported by EOW for the requested day
+                continue
+            except (EyeOnWaterAPIError, EyeOnWaterAuthError) as error:
+                raise UpdateFailed(error) from error
+
+            for row in data:
+                _LOGGER.debug(row)
+                statistics.append(
+                    StatisticData(
+                        start=row["start"],
+                        sum=row["sum"],
+                        min=row["sum"],
+                        max=row["sum"],
+                    )
+                )
+
+        return statistics

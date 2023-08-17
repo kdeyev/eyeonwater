657d5c09e9eaf90d31ed9d868e1ca42235e13096
Konstantin Deev <konstantinde@zillowgroup.com>
2023-08-17 00:05:46 -0500

    remove redundant fields

--- custom_components/eyeonwater/coordinator.py
+++ custom_components/eyeonwater/coordinator.py
@@ -115,13 +115,12 @@ class EyeOnWaterData:
 
             for row in data:
                 _LOGGER.debug(row)
-                statistics.append(
-                    StatisticData(
-                        start=row["start"],
-                        sum=row["sum"],
-                        min=row["sum"],
-                        max=row["sum"],
+                if row["sum"] > 0:
+                    statistics.append(
+                        StatisticData(
+                            start=row["start"],
+                            sum=row["sum"],
+                        )
                     )
-                )
 
         return statistics

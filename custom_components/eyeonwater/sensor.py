cebcf1f2869679a994bb863bfd8647c60b802fb5
Konstantin Deev <konstantinde@zillowgroup.com>
2023-08-20 12:39:59 -0500

    do not specify _attr_state_class

--- custom_components/eyeonwater/sensor.py
+++ custom_components/eyeonwater/sensor.py
@@ -31,7 +31,9 @@ class EyeOnWaterSensor(CoordinatorEntity, SensorEntity):
     _attr_has_entity_name = True
     _attr_name = None
     _attr_device_class = SensorDeviceClass.WATER
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    # We should not specify the state_class for workarounding the #30 issue
+    # _attr_state_class = SensorStateClass.TOTAL_INCREASING
 
     def __init__(self, meter: Meter, coordinator: DataUpdateCoordinator) -> None:
         """Initialize the sensor."""

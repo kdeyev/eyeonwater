ac12e2d77c6083d5eb17c740be97354b74be8642
Konstantin Deev <kn.deev@gmail.com>
2023-09-07 22:05:11 -0500

    add

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for eyeonwater tests."""
+
+# from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+
+
+CONF_PASSWORD: str = "password"
+CONF_USERNAME: str = "username"
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}

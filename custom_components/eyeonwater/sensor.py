ba71a9738b5ffdefcd8f6dcfbe635ab88cfdcf66
disforw <github@benzona.net>
2023-09-10 19:01:35 -0400

    Update sensor.py - revert DeviceInfo

--- custom_components/eyeonwater/sensor.py
+++ custom_components/eyeonwater/sensor.py
@@ -12,7 +12,8 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.device_registry import DeviceInfo
+#from homeassistant.helpers.device_registry import DeviceInfo   See issue #67
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,

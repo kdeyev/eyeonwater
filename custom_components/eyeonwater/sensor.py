4b5b6a1c49ff87e6f79ea33d9a210eeb111a0eb6
Kostya Deev <kn.deev@gmail.com>
2024-02-14 08:53:38 -0600

    Fully restore the workaround

--- custom_components/eyeonwater/sensor.py
+++ custom_components/eyeonwater/sensor.py
@@ -1,7 +1,7 @@
 """Support for EyeOnWater sensors."""
 import datetime
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pyonwater
 from homeassistant import exceptions
@@ -57,6 +57,7 @@ async def async_setup_entry(
                 last_imported_time=last_imported_time,
             ),
         )
+        sensors.append(EyeOnWaterSensor(meter, coordinator))
         sensors.append(EyeOnWaterTempSensor(meter, coordinator))
 
     async_add_entities(sensors, update_before_add=False)
@@ -84,7 +85,9 @@ class EyeOnWaterStatistic(CoordinatorEntity, SensorEntity):
 
         self._attr_name = f"{WATER_METER_NAME} {self.meter.meter_id} Statistic"
         self._attr_device_class = SensorDeviceClass.WATER
-        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+        # state class property is commented out due to a bug in HA, that generates negative statistics values
+
         self._attr_unique_id = f"{self.meter.meter_uuid}_statistic"
         self._attr_native_unit_of_measurement = get_ha_native_unit_of_measurement(
             meter.native_unit_of_measurement,
@@ -186,3 +189,71 @@ class EyeOnWaterTempSensor(CoordinatorEntity, SensorEntity):
     def native_value(self) -> float | None:
         """Get native value."""
         return self.meter.meter_info.sensors.endpoint_temperature.seven_day_min
+
+
+class EyeOnWaterSensor(CoordinatorEntity, SensorEntity):
+    """Representation of an EyeOnWater sensor."""
+
+    _attr_has_entity_name = True
+    _attr_name = None
+    _attr_device_class = SensorDeviceClass.WATER
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    def __init__(
+        self,
+        meter: pyonwater.Meter,
+        coordinator: DataUpdateCoordinator,
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self.meter = meter
+        self._state: pyonwater.DataPoint | None = None
+        self._available = False
+
+        self._attr_unique_id = meter.meter_uuid
+        self._attr_native_unit_of_measurement = get_ha_native_unit_of_measurement(
+            meter.native_unit_of_measurement,
+        )
+        self._attr_suggested_display_precision = 0
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self.meter.meter_uuid)},
+            name=f"{WATER_METER_NAME} {self.meter.meter_id}",
+            model=self.meter.meter_info.reading.model,
+            manufacturer=self.meter.meter_info.reading.customer_name,
+            hw_version=self.meter.meter_info.reading.hardware_version,
+            sw_version=self.meter.meter_info.reading.firmware_version,
+        )
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._available
+
+    @property
+    def native_value(self):
+        """Get the latest reading."""
+        return self._state.reading
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any]:
+        """Return the device specific state attributes."""
+        return self.meter.meter_info.reading.dict()
+
+    @callback
+    def _state_update(self):
+        """Call when the coordinator has an update."""
+        self._available = self.coordinator.last_update_success
+        if self._available:
+            self._state = self.meter.reading
+        self.async_write_ha_state()
+
+    async def async_added_to_hass(self):
+        """Subscribe to updates."""
+        self.async_on_remove(self.coordinator.async_add_listener(self._state_update))
+
+        if self.coordinator.last_update_success:
+            return
+
+        if last_state := await self.async_get_last_state():
+            self._state = last_state.state
+            self._available = True

ad804f0edcf21a303d92714e073bce8987427de1
Kostya Deev <kn.deev@gmail.com>
2025-01-07 18:16:45 -0600

    Remove Statistics workaround

--- custom_components/eyeonwater/sensor.py
+++ custom_components/eyeonwater/sensor.py
@@ -52,13 +52,12 @@ async def async_setup_entry(
         last_imported_time = await get_last_imported_time(hass, meter)
 
         sensors.append(
-            EyeOnWaterStatistic(
+            EyeOnWaterSensor(
                 meter,
                 coordinator,
                 last_imported_time=last_imported_time,
             ),
         )
-        sensors.append(EyeOnWaterSensor(meter, coordinator))
         if meter.meter_info.sensors and meter.meter_info.sensors.endpoint_temperature:
             sensors.append(EyeOnWaterTempSensor(meter, coordinator))
 
@@ -69,8 +68,13 @@ class NoDataFound(exceptions.HomeAssistantError):
     """Error to indicate there is no data."""
 
 
-class EyeOnWaterStatistic(CoordinatorEntity, SensorEntity):
+class EyeOnWaterSensor(CoordinatorEntity, SensorEntity):
     """Representation of an EyeOnWater sensor."""
+
+    _attr_has_entity_name = True
+    _attr_name = None
+    _attr_device_class = SensorDeviceClass.WATER
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
 
     def __init__(
         self,
@@ -86,11 +90,8 @@ class EyeOnWaterStatistic(CoordinatorEntity, SensorEntity):
 
         self._state: pyonwater.DataPoint | None = None
         self._available = False
-        self._historical_sensor = True
 
-        self._attr_name = f"{WATER_METER_NAME} {self._id} Statistic"
-        self._attr_device_class = SensorDeviceClass.WATER
-        self._attr_unique_id = f"{self._uuid}_statistic"
+        self._attr_unique_id = self._uuid
         self._attr_native_unit_of_measurement = get_ha_native_unit_of_measurement(
             meter.native_unit_of_measurement,
         )
@@ -115,6 +116,11 @@ class EyeOnWaterStatistic(CoordinatorEntity, SensorEntity):
     def native_value(self):
         """Get the latest reading."""
         return self._state.reading
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any]:
+        """Return the device specific state attributes."""
+        return self.meter.meter_info.reading.dict()
 
     @callback
     def _state_update(self):
@@ -200,74 +206,3 @@ class EyeOnWaterTempSensor(CoordinatorEntity, SensorEntity):
             return self.meter.meter_info.sensors.endpoint_temperature.seven_day_min
 
         return None
-
-
-class EyeOnWaterSensor(CoordinatorEntity, SensorEntity):
-    """Representation of an EyeOnWater sensor."""
-
-    _attr_has_entity_name = True
-    _attr_name = None
-    _attr_device_class = SensorDeviceClass.WATER
-    _attr_state_class = SensorStateClass.TOTAL_INCREASING
-
-    def __init__(
-        self,
-        meter: pyonwater.Meter,
-        coordinator: DataUpdateCoordinator,
-    ) -> None:
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.meter = meter
-        self._uuid = normalize_id(meter.meter_uuid)
-        self._id = normalize_id(meter.meter_id)
-
-        self._state: pyonwater.DataPoint | None = None
-        self._available = False
-
-        self._attr_unique_id = self._uuid
-        self._attr_native_unit_of_measurement = get_ha_native_unit_of_measurement(
-            meter.native_unit_of_measurement,
-        )
-        self._attr_suggested_display_precision = 0
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._uuid)},
-            name=f"{WATER_METER_NAME} {self._id}",
-            model=self.meter.meter_info.reading.model,
-            manufacturer=self.meter.meter_info.reading.customer_name,
-            hw_version=self.meter.meter_info.reading.hardware_version,
-            sw_version=self.meter.meter_info.reading.firmware_version,
-        )
-
-    @property
-    def available(self):
-        """Return True if entity is available."""
-        return self._available
-
-    @property
-    def native_value(self):
-        """Get the latest reading."""
-        return self._state.reading
-
-    @property
-    def extra_state_attributes(self) -> dict[str, Any]:
-        """Return the device specific state attributes."""
-        return self.meter.meter_info.reading.dict()
-
-    @callback
-    def _state_update(self):
-        """Call when the coordinator has an update."""
-        self._available = self.coordinator.last_update_success
-        if self._available:
-            self._state = self.meter.reading
-        self.async_write_ha_state()
-
-    async def async_added_to_hass(self):
-        """Subscribe to updates."""
-        self.async_on_remove(self.coordinator.async_add_listener(self._state_update))
-
-        if self.coordinator.last_update_success:
-            return
-
-        if last_state := await self.async_get_last_state():
-            self._state = last_state.state
-            self._available = True

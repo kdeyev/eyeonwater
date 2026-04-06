23dfdceb58a1e44e7ed3dc21669abac8cd9313e3
Kostya Deev <kn.deev@gmail.com>
2026-04-06 15:03:57 -0500

    chore: improve diagnostic logging in coordinator and init

--- custom_components/eyeonwater/coordinator.py
+++ custom_components/eyeonwater/coordinator.py
@@ -57,6 +57,10 @@ class EyeOnWaterData:
             CONF_PREFER_NEW_SEARCH,
             False,
         )
+        _LOGGER.debug(
+            "Setting up meters (prefer_new_search=%s)",
+            prefer_new_search,
+        )
         self.meters = await self.account.fetch_meters(
             self.client,
             prefer_new_search=prefer_new_search,
@@ -64,6 +68,12 @@ class EyeOnWaterData:
         _LOGGER.debug("Discovered %i meter(s)", len(self.meters))
 
         for meter in self.meters:
+            _LOGGER.debug(
+                "Meter: id=%s, uuid=%s, unit=%s",
+                meter.meter_id,
+                meter.meter_uuid,
+                meter.native_unit_of_measurement,
+            )
             self._last_imported_times[meter.meter_id] = await get_last_imported_time(
                 self.hass,
                 meter,
@@ -74,10 +84,11 @@ class EyeOnWaterData:
         for meter in self.meters:
             try:
                 await meter.read_meter_info(client=self.client)
-            except EyeOnWaterException:
+            except EyeOnWaterException as exc:
                 _LOGGER.warning(
-                    "Failed to refresh meter info for %s, using cached data",
+                    "Failed to refresh meter info for %s, using cached data: %s",
                     meter.meter_id,
+                    exc,
                 )
 
             try:
@@ -85,10 +96,11 @@ class EyeOnWaterData:
                     client=self.client,
                     days_to_load=days_to_load,
                 )
-            except EyeOnWaterException:
+            except EyeOnWaterException as exc:
                 _LOGGER.warning(
-                    "Failed to read historical data for %s",
+                    "Failed to read historical data for %s: %s",
                     meter.meter_id,
+                    exc,
                 )
 
             self._import_meter_statistics(meter)
@@ -112,6 +124,7 @@ class EyeOnWaterData:
     def _import_meter_statistics(self, meter: Meter) -> None:
         """Filter and import new historical data points for a meter."""
         if not meter.last_historical_data:
+            _LOGGER.debug("No historical data for meter %s", meter.meter_id)
             return
 
         last_imported = self._last_imported_times.get(meter.meter_id)
@@ -120,7 +133,11 @@ class EyeOnWaterData:
         if not new_data:
             return
 
-        _LOGGER.info("%i data points will be imported", len(new_data))
+        _LOGGER.info(
+            "%i data points will be imported for meter %s",
+            len(new_data),
+            meter.meter_id,
+        )
         factor = self._get_volume_factor(meter)
         statistics = convert_statistic_data(new_data, factor)
         metadata = get_statistic_metadata(meter, self._get_display_unit())
@@ -175,7 +192,11 @@ class EyeOnWaterData:
                 _LOGGER.info("No historical data for meter %s", meter.meter_id)
                 continue
 
-            _LOGGER.info("%i data points will be imported", len(data))
+            _LOGGER.info(
+                "%i historical data points will be imported for meter %s",
+                len(data),
+                meter.meter_id,
+            )
             factor = self._get_volume_factor(meter)
             statistics = convert_statistic_data(data, factor)
             metadata = get_statistic_metadata(meter, self._get_display_unit())
